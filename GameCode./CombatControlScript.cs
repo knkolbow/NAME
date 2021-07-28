0c0b13cc5e516539abeb8ac26ff1cfd66f6f166c
knkolbow <73507371+knkolbow@users.noreply.github.com>
2021-07-28 02:25:40 -0500

    Update CombatControlScript.cs

--- GameCode./CombatControlScript.cs
+++ GameCode./CombatControlScript.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Firebase.Database;
+using Firebase.Auth;
 
 public class CombatControlScript : MonoBehaviour
 {
